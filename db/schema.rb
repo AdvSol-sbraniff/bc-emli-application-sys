c7cbf0c9ad2f51138fa321ae9839e25f8ee85d70
Stephen Braniff <sbraniff@5CG3386M88-N.corp.advancedsolutions.eds.com>
2026-04-26 12:07:15 -0700

    Take upstream schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_01_22_175531) do
-  create_schema "claims"
-
+ActiveRecord::Schema[7.1].define(version: 2026_02_19_222612) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
